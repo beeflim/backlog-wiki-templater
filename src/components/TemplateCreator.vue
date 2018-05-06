5f35bd7a605289d3112c6abfb23fd87b60aafceb
k-takahashi <k-takahsahi@sig-c.co.jp>
2018-05-06 21:51:44 +0900

    [add] 名前の修正

--- src/components/TemplateCreator.vue
+++ src/components/TemplateCreator.vue
@@ -1,12 +1,26 @@
 <template>
   <b-container class="bv-example-row">
+    <div class="alert-area">
+      <ui-alert @dismiss="showAlert1 = false" v-show="showAlert1">
+        backlogの”テキストの生成ルール”がBacklogになっている事を確認してください。
+        <a href="https://backlog.com/ja/help/adminsguide/project-setting/userguide355/"
+           target="_blank">backlogの設定 <i
+          class="material-icons material-icons__10">filter_none</i></a>
+      </ui-alert>
+      <ui-alert @dismiss="showAlert2 = false" v-show="showAlert2">
+        入力チェックは最低限しかしていないので、上から順に優しく入力してください。
+      </ui-alert>
+    </div>
+    <ui-modal ref="successModal" title="完了">
+      {{errorMsg}}
+    </ui-modal>
     <ui-modal ref="errorModal" title="エラー">
       {{errorMsg}}
     </ui-modal>
     <b-row>
       <b-col>
         <section>
-          <h1>作成するWikiの設定</h1>
+          <h1>Wikiの作成</h1>
           <div class="input-box">
             <ui-textbox
               floating-label
@@ -22,7 +36,9 @@
             ></ui-textbox>
             <div class="help-message">
             <span>
-              <a href='https://backlog.com/ja/support/general/what-id/'>スペースIDとは</a>
+              <a href='https://backlog.com/ja/support/general/what-id/' target="_blank">スペースIDとは
+                <i class="material-icons material-icons__10">filter_none</i>
+              </a>
             </span>
             </div>
           </div>
@@ -41,7 +57,8 @@
             ></ui-textbox>
             <div class="help-message">
             <span>
-              <a href='https://backlog.com/ja/help/usersguide/personal-settings/userguide2378'>API Keyとは</a>
+              <a href='https://backlog.com/ja/help/usersguide/personal-settings/userguide2378' target="_blank">API Keyとは <i
+                class="material-icons material-icons__10">filter_none</i></a>
             </span>
             </div>
           </div>
@@ -119,6 +136,18 @@
               </p>
               <b-button href="https://material.io/icons/" variant="primary">Material Icon</b-button>
             </b-card>
+            <b-card title="Vue.js"
+                    img-src="static/vue.png"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    style="max-width: 20rem;"
+                    class="mb-2">
+              <p class="card-text">
+                全体で使用しています。
+              </p>
+              <b-button href="https://jp.vuejs.org/v2/guide/index.html" variant="primary">Vue.js</b-button>
+            </b-card>
           </b-card-group>
         </section>
       </b-col>
@@ -129,20 +158,29 @@
 
 <script>
   import axios from 'axios'
+  import wikiData from '../data/wiki'
 
   export default {
-    name: 'HelloWorld',
+    name: 'TemplateCreator',
     data() {
       return {
         spaceId: '',　                 //スペースID
         spaceIdTouched: false,　       //スペースIDのエラーハンドリング用
-        userApiKey: '',　              //Apiキー
+        userApiKey: '',　      　      //Apiキー
         userApiKeyTouched: false,     //Apiキーのエラーハンドリング用
         targetProject: '',            //対象プロジェクト
         mileStoneName: '',            //マイルストーン名
         mileStoneNameTouched: false,  //マイルストーン名のエラーハンドリング用
         projectNames: [],             //プロジェクト名のリスト表示用
-        errorMsg: ''                  //モーダルのエラーメッセージ
+        errorMsg: '',                 //モーダルのエラーメッセージ
+        showAlert1: true,
+        showAlert2: true
+      }
+    },
+    computed: {
+      //Wikiのデータ
+      wikiList: function () {
+        return wikiData.getData(this.mileStoneName)
       }
     },
     methods: {
@@ -150,13 +188,14 @@
        * プロジェクト名を取得してセレクトに代入する
        */
       getProjectNames: function () {
+        this.projectNames = [];
+        this.targetProject = '';
+
         // 最低条件が入っていない時は処理しない
         if (this.spaceId == "" || this.userApiKey == "" || this.userApiKey.length != 64) {
           console.log('スペースIDかAPIキーが未入力なので処理しない')
           return;
         }
-
-        this.projectNames = []
 
         //値を取得してくる
         axios.get(createAxiosUrl(this.spaceId, this.userApiKey, 'projects'))
@@ -179,6 +218,7 @@
             console.log(error);
             this.openModal('errorModal', 'プロジェクト一覧の取得に失敗しました');
           });
+
       },
       /**
        * モーダルを表示する
@@ -193,26 +233,49 @@
        * wikiに定型文を投稿する
        */
       postWikiTemplate: function () {
+
         //すべての入力が行われていることを確認する。
         if (this.spaceId == "" || this.userApiKey == '' || this.targetProject.value == '' || this.mileStoneName == '') {
           return;
         }
 
-        const backlog = axios.create()
+        axios.all(createMultiAxios(this.wikiList, this.spaceId, this.userApiKey, this.targetProject.value, this.mileStoneName))
+          .then(([api1Result]) => {
 
-        axios.all([
-          backlog.post(createAxiosUrl(this.spaceId, this.userApiKey, 'wikis'), createWikiRequest(this.targetProject.value, this.mileStoneName, '', '# test page'))
-        ]).then(([api1Result]) => {
-          console.log(api1Result)
-          alert('作成した')
+            console.log(api1Result)
+            this.openModal('successModal', 'Wikiの作成が終わりました');
 
-        }).catch(error => {
-          //エラー表示
-          console.log(error);
-          this.openModal('errorModal', 'Wikiの作成に失敗しました');
-        });
+          })
+          .catch(error => {
+
+            //エラー表示
+            console.log(error);
+            this.openModal('errorModal', 'Wikiの作成に失敗しました');
+          });
       }
     }
+  }
+
+  /**
+   * 非同期でwiki文章を作成する
+   *
+   * @param spaceId {string} スペースID
+   * @param userApiKey {string} API Key
+   * @param targetProject {string} プロジェクトID
+   * @param mileStoneName {string} マイルストーン名
+   */
+  function createMultiAxios(wikiList, spaceId, userApiKey, targetProject, mileStoneName) {
+    const backlog = axios.create()
+    let axiosList = []
+
+    //リストの内容分表示する
+    let list = wikiList;
+
+    list.forEach(wiki => {
+      axiosList.push(backlog.post(createAxiosUrl(spaceId, userApiKey, 'wikis'), createWikiRequest(targetProject, mileStoneName, wiki.name, wiki.content)));
+    });
+    console.log(axiosList)
+    return axiosList;
   }
 
   /**
@@ -255,9 +318,12 @@
    * @return {URLSearchParams} param
    */
   function createWikiRequest(targetProject, mileStoneName, name, content) {
+
+    let pageName = name == '' ? mileStoneName : mileStoneName + name
+
     var params = new URLSearchParams();
     params.append('projectId', targetProject);
-    params.append('name', mileStoneName);
+    params.append('name', pageName);
     params.append('content', content);
     params.append('mailNotify', 'false');
     return params;
@@ -275,15 +341,18 @@
     background: linear-gradient(transparent 90%, #46C997);
   }
 
+  /* セクションの調整 */
   section {
     margin-top: 5vh;
     margin-bottom: 5vh;
   }
 
+  /* 参考サイトの調整 */
   .site-area {
     margin-top: 20vh;
   }
 
+  /* 説明のための外部リンク */
   .help-message {
     span {
       font-size: 12px;
@@ -294,12 +363,23 @@
       content: "※";
       color: #42b983;
     }
+
   }
 
+  /* inputの間隔調整 */
   .input-box {
     margin-bottom: 30px;
   }
-  .mb-2 a{
+
+  /* 参考リンクのボタンの色 */
+  .mb-2 a {
     color: #f0f0f0;
+  }
+
+  .material-icons__10 {
+    font-size: 10px;
+  }
+  .alert-area{
+    margin-top: 30px;
   }
 </style>
--- test/unit/specs/TemplateCreator.spec.js
+++ test/unit/specs/TemplateCreator.spec.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import HelloWorld from '@/components/HelloWorld'
+import TemplateCreator from '@/components/TemplateCreator'
 
-describe('HelloWorld.vue', () => {
+describe('TemplateCreator.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(HelloWorld)
+    const Constructor = Vue.extend(TemplateCreator)
     const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to Your Vue.js App')
